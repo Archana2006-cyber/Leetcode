ca3f65add1931b4139e14631252751de59a68ad8
Archana2006-cyber <i.m.archana2006@gmail.com>
2026-03-18 12:13:25 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (91.03%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -20,9 +20,9 @@ class Solution {
         return result;
     }
     void preorder(TreeNode root, List<Integer> result) {
-        if (root == null) return;
-        result.add(root.val);        
+        if (root == null) return; 
+        result.add(root.val);      
         preorder(root.left, result);
-        preorder(root.right, result);
+        preorder(root.right, result); 
     }
 }
