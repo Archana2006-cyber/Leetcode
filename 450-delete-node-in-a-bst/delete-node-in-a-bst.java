e384364f4892e55b3dfe742e77da83718670e294
Archana2006-cyber <i.m.archana2006@gmail.com>
2026-03-23 13:49:19 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (85.40%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,37 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } 
+        else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } 
+        else {
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            else if (root.right == null) {
+                return root.left;
+            } 
+            else if (root.left == null) {
+                return root.right;
+            } 
+            else {
+                TreeNode temp = findmin(root.right);
+                root.val = temp.val;
+                root.right = deleteNode(root.right, temp.val);
+            }
+        }
+        return root;
+    }
+
+    private TreeNode findmin(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+}
