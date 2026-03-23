1d617ebedd23af851698be8401476c848c30dfec
Archana2006-cyber <i.m.archana2006@gmail.com>
2026-03-23 14:39:44 +0530

    Time: 2 ms (12.37%) | Memory: 45.4 MB (29.41%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,14 +13,39 @@
  *     }
  * }
  */
+
+ /*
 class Solution {
     public boolean isValid(TreeNode root, long min, long max) {
-        if (root == null) return true;
-        if (root.val <= min || root.val >= max) return false;
-        return isValid(root.left, min, root.val) &&
+        if (root==null) 
+        return true;
+        if (root.val<=min||root.val >=max) return false;
+        return isValid(root.left,min,root.val) &&
                isValid(root.right, root.val, max);
     }
     public boolean isValidBST(TreeNode root) {
-        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+}
+*/
+import java.util.*;
+
+class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) <= list.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
     }
 }
